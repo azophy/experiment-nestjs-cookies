f72b3b2c6ae49268cd0997c645e4eac1fc3ed47c
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-12-18 17:09:12 +0700

    add sample cookie endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express'
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/cek_cookies')  
+  getCekCookies(@Req() req: Request): string {
+    return JSON.stringify(req.cookies)
+  }
+
+  @Get('/set_cookies')  
+  getSetCookies(@Res() res: Response): string {
+    console.log('masuk')
+    res.cookie('coba', Date.now())
+     return JSON.stringify({ status: 'ok'})
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+// somewhere in your initialization file
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
