911158894ea1fb9d9f6b788b7ebce21f4446dc09
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-12-20 16:43:02 +0700

    try enabling httpOnly without any further config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,15 +18,16 @@ export class AppController {
   @Get('/set_cookies')  
   getSetCookies(@Res({ passthrough: true }) res: Response): string {
     console.log('masuk')
-    const cookieDomain = process.env.COOKIE_DOMAIN || null
+    //const cookieDomain = process.env.COOKIE_DOMAIN || null
 
-    const cookieOptions: CookieOptions = cookieDomain 
-    ? { 
-      domain: cookieDomain,
-      httpOnly: true,
-      sameSite: "none",
-    }
-    : {}
+    //const cookieOptions: CookieOptions = cookieDomain 
+    //? { 
+      //domain: cookieDomain,
+      //httpOnly: true,
+      //sameSite: "none",
+    //}
+    //: {}
+    const cookieOptions = { httpOnly: true }
 
     res.cookie('coba', Date.now().toString(), cookieOptions)
     
